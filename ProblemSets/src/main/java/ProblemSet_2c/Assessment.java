a3fc32d04a9d1ed16bd6856be934a16d63bf78db
Malik <am05550@heron010.eps.surrey.ac.uk>
2024-10-21 23:18:19 +0100

    Completed

---
+++ ProblemSets/src/main/java/ProblemSet_2c/Assessment.java
@@ -0,0 +1,23 @@
+package ProblemSet_2c;
+
+public class Assessment {
+	private String name = null;
+	private double mark;
+	
+	public Assessment(String assessmentName, double mark) {
+		this.name = assessmentName;
+		this.mark = mark;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getMark() {
+		return mark;
+	}
+	public void setMark(double mark) {
+		this.mark = mark;
+	}
+}
---
+++ ProblemSets/src/main/java/ProblemSet_2c/Module.java
@@ -0,0 +1,22 @@
+package ProblemSet_2c;
+
+public class Module {
+	private String NAME = "COM1027";
+	private Assessment assessment1;
+	private Assessment assessment2;
+	private Assessment assessment3;
+	
+	public Module(Assessment assessment1, Assessment assessment2, Assessment assessment3) {
+		this.assessment1 = assessment1;
+		this.assessment2 = assessment2;
+		this.assessment3 = assessment3;	
+	}
+	public double calculateAverage() {
+		return(assessment1.getMark() * 0.2) + (assessment2.getMark() * 0.2) + (assessment3.getMark() * 0.6);
+	}
+	@Override
+	public String toString() {
+		return NAME + " (" + calculateAverage() + "%)";
+	}
+	
+}
