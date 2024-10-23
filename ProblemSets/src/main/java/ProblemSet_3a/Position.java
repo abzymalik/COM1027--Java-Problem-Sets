da2d8239477ab53614885b9201059af3ab9b10ea
Malik <am05550@heron010.eps.surrey.ac.uk>
2024-10-23 16:51:37 +0100

    Completed

---
+++ ProblemSets/src/main/java/ProblemSet_3a/Position.java
@@ -0,0 +1,13 @@
+package ProblemSet_3a;
+
+public class Position {
+	private String role = null;
+	
+	public String getRoleName() {
+		return role;
+	}
+	public void setRoleName(String role) {
+		this.role = role;
+	}
+
+}
