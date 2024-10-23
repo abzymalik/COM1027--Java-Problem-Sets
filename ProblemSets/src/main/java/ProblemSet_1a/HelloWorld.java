a20f515fa7f29925e9f0f20e23fb8df3d3476dff
Malik <am05550@heron010.eps.surrey.ac.uk>
2024-10-23 17:43:00 +0100

    Completed

--- ProblemSets/src/main/java/ProblemSet_1a/HelloWorld.java
+++ ProblemSets/src/main/java/ProblemSet_1a/HelloWorld.java
@@ -1,9 +1,9 @@
 package ProblemSet_1a;
 
 public class HelloWorld {
-    // Private attribute/field called "text", which is being initialised to null or empty.
-	private String text = null; 
-	// Constructor for the HelloWorld class which takes a String as a parameter.
+    // Private attribute called "text", which is being initialised to null.
+	private String text = null;
+	// Constructor for the class which takes a String as a parameter.
     // The constructor is called when an object of the class is created.
 	public HelloWorld(String string) {
 		text = string;
@@ -11,6 +11,6 @@ public class HelloWorld {
 	// Method to return the value of the private attribute "text".
 	public String display() {
 		// Return the value stored in the "text" variable.
-		return text; 
+		return text;
 	}
 }
