c46879357be32020b307969bc6cc750dd09a5dfc
Malik <am05550@heron010.eps.surrey.ac.uk>
2024-10-23 17:21:27 +0100

    Completed

--- ProblemSets/src/main/java/ProblemSet_2b/Meal.java
+++ ProblemSets/src/main/java/ProblemSet_2b/Meal.java
@@ -1,19 +1,15 @@
 package ProblemSet_2b;
 
 public class Meal {
-	
 	private Food mainCourse;
 	private Food dessert;
-	
 	public Meal(Food mainCourse, Food dessert) {
 		this.mainCourse = mainCourse;
 		this.dessert = dessert;
 	}
-	
 	public double calculateTotalSugarLevel() {
 		return mainCourse.getAmountOfSugar() + dessert.getAmountOfSugar();
 	}
-	
 	public String toString() {
 		return "Meal [mainCourse = " + mainCourse.getName() + ", dessert = " + dessert.getName() + "]";
 	}
