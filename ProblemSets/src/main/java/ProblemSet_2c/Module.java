3898adc1abdebdcd74cce8c7f60ee3824df341bf
Malik <am05550@heron189.eps.surrey.ac.uk>
2024-10-29 15:44:49 +0000

    Completed

--- ProblemSets/src/main/java/ProblemSet_2c/Module.java
+++ ProblemSets/src/main/java/ProblemSet_2c/Module.java
@@ -5,18 +5,18 @@ public class Module {
 	private Assessment assessment1;
 	private Assessment assessment2;
 	private Assessment assessment3;
-	
-	public Module(Assessment assessment1, Assessment assessment2, Assessment assessment3) {
+	public Module(Assessment assessment1,
+			Assessment assessment2, Assessment assessment3) {
 		this.assessment1 = assessment1;
 		this.assessment2 = assessment2;
 		this.assessment3 = assessment3;	
 	}
 	public double calculateAverage() {
-		return(assessment1.getMark() * 0.2) + (assessment2.getMark() * 0.2) + (assessment3.getMark() * 0.6);
+		return (assessment1.getMark() * 0.2) + (assessment2.getMark() * 0.2)
+				+ (assessment3.getMark() * 0.6);
 	}
 	@Override
 	public String toString() {
 		return NAME + " (" + calculateAverage() + "%)";
 	}
-	
 }
