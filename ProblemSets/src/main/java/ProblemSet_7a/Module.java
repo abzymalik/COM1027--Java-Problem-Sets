65c9fe1f72d3817f082e8fc096a68a65c9021b5b
Malik <am05550@heron200.eps.surrey.ac.uk>
2024-11-11 14:13:18 +0000

    Completed

---
+++ ProblemSets/src/main/java/ProblemSet_7a/Module.java
@@ -0,0 +1,18 @@
+package ProblemSet_7a;
+
+public class Module {
+	private String name;
+	
+	public Module(String name) {
+		if (name.matches("COM\\d{4}")) {
+			this.name = name;
+		} else {
+			this.name = "Error";
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ ProblemSets/src/main/java/ProblemSet_7b/Student.java
@@ -0,0 +1,12 @@
+package ProblemSet_7b;
+
+public class Student {
+	private String name;
+	private String urn;
+	private list<Module> moduleList;
+	
+	public Student(String name, String urn) {
+		if (name.matches("A"))
+	}
+
+}
