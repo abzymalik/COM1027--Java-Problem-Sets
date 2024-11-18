bc18f5368c630e65cccafc071f576e899420750f
Malik <am05550@heron006.eps.surrey.ac.uk>
2024-11-18 22:56:59 +0000

    Completed

--- ProblemSets/src/main/java/ProblemSet_8a/Customer.java
+++ ProblemSets/src/main/java/ProblemSet_8a/Customer.java
@@ -3,21 +3,21 @@ package ProblemSet_8a;
 public class Customer {
 	private String name;
 	private String surname;
-	
+
 	public Customer(String name, String surname) {
 		super();
 		if (validateInfo(name) && validateInfo(surname)) {
 			this.name = name;
 			this.surname = surname;
 			} else {
-				throw new IllegalArgumentException("Invalid name and/or surname format");
+				throw new IllegalArgumentException(
+						"Invalid name and/or surname format");
 			}
 	}
-	
+
 	public char getName() {
 		return name.charAt(0);
 	}
-	
 
 	public String getSurname() {
 		return surname;
@@ -30,8 +30,7 @@ public class Customer {
 	public boolean validateInfo(String info) {
 		return info.matches("[A-Z][a-z]*");
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return  getName() + ". " + surname;
