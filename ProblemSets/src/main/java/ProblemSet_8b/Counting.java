1438428689bd4140c5487ef3dd19d35e9450a14e
Malik <am05550@heron180.eps.surrey.ac.uk>
2024-11-27 00:03:50 +0000

    Completed

--- ProblemSets/src/main/java/ProblemSet_8b/Counting.java
+++ ProblemSets/src/main/java/ProblemSet_8b/Counting.java
@@ -3,31 +3,26 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Counting{
-	public String readTextFile(String filename)
-{
+public class Counting {
+	public String readTextFile(String filename){
 		StringBuilder content = new StringBuilder();
-		int counter=1;
-		try(BufferedReader reader = new BufferedReader(new FileReader(filename))){
+		int counter = 1;
+		try (BufferedReader reader = new BufferedReader
+				(new FileReader(filename))) {
 			String line;
-			while((line = reader.readLine())!=null) {
+			while ((line = reader.readLine()) != null) {
 				content.append(displayLine(line, counter)).append("\n");
 				counter++;
 			}
 		}
-		catch(IOException e) {
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 		return content.toString();
 		
 }
 	public String displayLine(String line, int count) {
-		return line + " : "+count;
+		return line + " : " + count;
 	}
 }
-class Main{
-	public static void main(String[] args) {
-		Counting c= new Counting();
-		String content = c.readTextFile("words.txt");
-	}
-}
+
