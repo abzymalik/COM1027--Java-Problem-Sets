95c1093523bed525b3359a47fc3c3d158379282b
Malik <am05550@heron180.eps.surrey.ac.uk>
2024-11-27 00:09:08 +0000

    Completed

--- ProblemSets/src/main/java/ProblemSet_8b/Counting.java
+++ ProblemSets/src/main/java/ProblemSet_8b/Counting.java
@@ -4,11 +4,11 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Counting {
-	public String readTextFile(String filename){
+	public String readTextFile(String filename) {
 		StringBuilder content = new StringBuilder();
 		int counter = 1;
-		try (BufferedReader reader = new BufferedReader
-				(new FileReader(filename))) {
+		try (BufferedReader reader = new BufferedReader(
+				new FileReader(filename))) {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				content.append(displayLine(line, counter)).append("\n");
@@ -19,7 +19,7 @@ public class Counting {
 			e.printStackTrace();
 		}
 		return content.toString();
-		
+
 }
 	public String displayLine(String line, int count) {
 		return line + " : " + count;
