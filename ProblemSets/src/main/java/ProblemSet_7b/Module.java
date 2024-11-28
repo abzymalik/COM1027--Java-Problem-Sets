2b215a6705b84f61434cba736ec045df5950e5c5
Malik <am05550@heron187.eps.surrey.ac.uk>
2024-11-28 15:36:23 +0000

    Completed

--- ProblemSets/src/main/java/ProblemSet_7b/Module.java
+++ ProblemSets/src/main/java/ProblemSet_7b/Module.java
@@ -2,16 +2,16 @@ package ProblemSet_7b;
 
 public class Module {
 	private String name;
-	
+
 	public Module(String name) {
 		super();
 		if (name.matches("COM\\d{4}")) {
-			this.name = name;  
+			this.name = name;
 		} else {
 			this.name = "Error";
 		}
 	}
-	
+
 	public String getName() {
 		return name;
 	}
