0561ac5dd104043c5613fe78231d306e3f6249e9
Malik <am05550@heron195.eps.surrey.ac.uk>
2024-12-03 17:29:45 +0000

    Completed

--- ProblemSets/src/main/java/ProblemSet_8c/Person.java
+++ ProblemSets/src/main/java/ProblemSet_8c/Person.java
@@ -1,5 +1,38 @@
 package ProblemSet_8c;
 
 public class Person {
+	private String forename;
+	private String surname;
+	private int age;
+	
+	/**
+	 * Constructs a Person object
+	 * 
+	 * @param name
+	 * @param surname
+	 * @param age
+	 */
+	public Person(String forename, String surname, int age) {
+		super();
+		this.forename = forename;
+		this.surname = surname;
+		this.age = age;
+	}
 
+	public String getForename() {
+		return forename;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s %s (%d)", this.forename, this.surname, this.age);
+	}
 }
--- ProblemSets/src/main/java/ProblemSet_8c/PersonTracker.java
+++ ProblemSets/src/main/java/ProblemSet_8c/PersonTracker.java
@@ -1,5 +1,81 @@
 package ProblemSet_8c;
 
-public class PersonTracker {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
+public class PersonTracker {
+	private List<Person> peopleList;
+	public PersonTracker() {
+		super();
+		this.peopleList = new ArrayList<Person>();
+	}
+	/**
+	 * Adds a Person object to Person list
+	 * 
+	 * @param f
+	 * @param s
+	 * @param a 
+	 */
+	
+	public void addPerson(String f, String s, int a) {
+		this.peopleList.add(new Person(f, s, a));
+	}
+	
+	/**
+	 * Reads the filename
+	 * 
+	 * @param filename of the filename to be read and checked]
+	 * @return contents of the file
+	 * @throws RunTimeException if file read fails
+	 */
+	
+	public String readTextFile(String file) throws RuntimeException {
+		StringBuilder toReturn = new StringBuilder();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = reader.readLine();
+			
+			while (line != null) {
+				this.breakLine(line);
+				toReturn.append(String.format("%s\n", line));
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException exception){
+			throw new RuntimeException(exception);
+		}
+		return toReturn.toString();
+	}
+	
+	/**
+	 * Parses the line and constructs a Person object to add to the liust
+	 * @param line
+	 */
+	
+	private void breakLine(String line) {
+		String[] parts = line.split(" ");
+		this.addPerson(parts[0], parts[1], Integer.parseInt(parts[2]));
+	}
+	
+	/**
+	 * Gets all the people present in the tracker and returns all the elements in the people list
+	 */
+	public String getPeopleList() {
+		return this.peopleList.toString();
+	}
+	/**
+	 * Returns the list of all people present in the tracker 
+	 * @return 
+	 */
+	
+	public String displayList() {
+		StringBuilder toReturn = new StringBuilder();
+		for (Person person : this.peopleList) {
+			toReturn.append(person.toString()).append("\n");
+		}
+		return toReturn.toString();
+	}
 }
